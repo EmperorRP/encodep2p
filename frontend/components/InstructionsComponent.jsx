885d60f3a6a29c5c26203d563e9cb550fc9f5b41
Sudarshan K V <kvsudharshan@gmail.com>
2023-06-15 23:28:38 +0530

    final commit

--- frontend/components/InstructionsComponent.jsx
+++ frontend/components/InstructionsComponent.jsx
@@ -6,13 +6,13 @@ export default function InstructionsComponent() {
     <div className={styles.container}>
       <header className={styles.header_container}>
         <h1>
-          <span>Encode P2P Group 2</span>
+          <span>Encode: P2P Xchange</span>
         </h1>
         <h2>Welcome to P2P crypto swap</h2>
         
      
      <br></br><br></br><br></br><br></br>
-     <h3><b><u>Team members:</u></b></h3><br></br>
+     <h3><b><u>Group II :</u></b></h3><br></br>
       <h3><i>Mahadevaswamy K S</i></h3>
       <h3><i>Sudarshan K V</i></h3>
       <h3><i>Rahul Pujari</i></h3>
